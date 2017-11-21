b065fbd8e8af38fdd1d9948273d77c9abcfe6138
cupururum <cupururum@Vogon-2.local>
2017-11-21 09:39:33 -0800

    finished

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,5 @@
 // problem webpage does not respond to the user
 // solution : add jQuery to improve user experience
-var randomNumber;
-var crystalsValue;
-var sumOfValues = 0;
-var randomNumberCrystalsValue;
 var wins = 0;
 var losses = 0;
 var $winsP = $("#winsP");
@@ -12,69 +8,70 @@ var randomNumberP = $("#randomNumberDisplay");
 var yourNumberP = $("#guessNumberDisplay")
 var $beginGame = $("#beginGame");
 
-function randomNumberCall() {
-  //the game gives random number that should appear in the "random number"
-    randomNumber = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
-    console.log(randomNumber);
-    randomNumberP.text(randomNumber);
-    yourNumberP.text(sumOfValues);
-}
-
-// when game begins for each cristal we assign a random value
-function randomNumberCrystals() {
-    $(".crystals").each(function(){
-      randomNumberCrystalsValue = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
-      $(this).attr("value", randomNumberCrystalsValue);
-      console.log("randomNumberCrystalsValue: " + randomNumberCrystalsValue)
-    });
-};
 
 //when user clicks button with crystal
   //the button should add the number in the "your number"
-function addValueFromTheCrystal(){
+
+var randomNumber;
+var randomNumberCrystalsValue;
+var crystalsValue;
+var sumOfValues = 0;
+
+function randomNumbersCall() {
+
+  sumOfValues = 0;
+
+  randomNumber = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+  console.log(randomNumber);
+  randomNumberP.text(randomNumber);
+  yourNumberP.text(sumOfValues);
+
+// when game begins for each cristal we assign a random value
+
+  $(".crystals").each(function(){
+    randomNumberCrystalsValue = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+    $(this).attr("value", randomNumberCrystalsValue);
+    console.log("randomNumberCrystalsValue: " + randomNumberCrystalsValue)
+  });
+
+};
+//the game gives random number that should appear in the "random number"
+randomNumbersCall();
 
     $(".crystals").on("click", function(){
-      crystalsValue = this.value;
+      crystalsValue = parseInt(this.value);
       console.log(crystalsValue);
-      crystalsValue = parseInt(crystalsValue);
       sumOfValues = sumOfValues + crystalsValue;
       console.log("sumOfValues: " + sumOfValues);
       yourNumberP.text(sumOfValues);
+      //check every time if the user riched the random number
+      if (sumOfValues === randomNumber) {
+           wins++;
+           $winsP.text("Wins: " + wins);
+           randomNumbersCall();
 
+      } else if (sumOfValues > randomNumber) {
+           losses++;
+           $lossesP.text("Losses: " + losses);
+           randomNumbersCall();
+
+      } else {
+        //ignore
+      }
     });
-};
 
-// function gameOver(){
-//   if (sumOfValues === randomNumber) {
-//        wins++;
-//        $winsP.text("Wins: " + wins);
-//        beginGame();
-//   } else if (sumOfValues > randomNumber) {
-//        losses++;
-//        $lossesP.text("Losses: " + losses);
-//        beginGame();
-//   } else {
-//     //ignore
-//   }
-// };
+
+
+
+// //when user presses the button "begin game"
+//   $beginGame.on("click", function(){
 //
-// gameOver();
+//
+//   });
+// };
 
 
 
-//when user presses the button "begin game"
-function beginGame() {
-  $beginGame.on("click", function(){
-    console.log(sumOfValues);
-    sumOfValues = 0;
-    $beginGame.text("Play Again");
-    randomNumberCall();
-    randomNumberCrystals();
-    addValueFromTheCrystal();
-  });
-};
-
-beginGame()
 
 
 
